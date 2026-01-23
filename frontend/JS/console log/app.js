b5aeff0b24d5de5d4fffb53f5a0686f88061fabe
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2026-01-23 17:14:55 +0530

    Add files via upload

---
+++ frontend/JS/console log/app.js
@@ -0,0 +1,8 @@
+console.log("Hello World !");
+console.log("Welcome to Javascript Programming");
+
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+console.log(sum);
---
+++ frontend/JS/template literals/app.js
@@ -0,0 +1,6 @@
+let pencilPrice = 10;
+let erasorPrice = 5;
+
+// let totalPrice = "The total price is : " + (pencilPrice + erasorPrice) + " Rupees. "
+let totalPrice = `The total price is : ${pencilPrice + erasorPrice} Rupees.`;
+console.log(totalPrice);
