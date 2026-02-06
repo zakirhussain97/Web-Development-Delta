03c9092118820c2badea33d6f0965e0924948bb8
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2026-02-06 18:51:25 +0530

    Practice

---
+++ frontend/JS/Practice Qs 2/app.js
@@ -0,0 +1,10 @@
+let size = "xl" 
+if (size === "xl") {
+    console.log("Price is Rs. 250")
+} else if (size === "l") {
+    console.log("Price is Rs. 200")
+} else if (size === "m") {
+    console.log("Price is Rs. 150")
+} else {
+    console.log("Price is Rs. 100")
+}
