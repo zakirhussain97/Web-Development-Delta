bb445c354202bfe997da1e4ac9718d4c73f14c7e
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2026-02-06 18:52:42 +0530

    nested if-else example

---
+++ frontend/JS/nested if-else/app.js
@@ -0,0 +1,12 @@
+let marks = 23;
+
+if (marks >= 33) {
+  console.log("Pass");
+  if (marks >= 80) {
+    console.log("Grade : O");
+  } else {
+    console.log("Grade : A");
+  }
+} else {
+  console.log("Fail");
+}
