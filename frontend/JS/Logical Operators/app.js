9706d0773439d6480bb02c62a498f7a29c0ff560
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2026-02-11 19:18:29 +0530

    Logical Operators example

---
+++ frontend/JS/Logical Operators/app.js
@@ -0,0 +1,13 @@
+// let marks = 78;
+
+// if( marks >=33 && marks >= 80) {
+//     console.log("Pass")
+//     console.log("Grade A")
+// }
+
+
+let marks = 23;
+
+if ((marks > 33 && marks <= 80) || !false) {
+    console.log("Pass");
+}
