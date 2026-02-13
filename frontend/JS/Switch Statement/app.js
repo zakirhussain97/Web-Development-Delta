74ae4bb7dfb4f09294e45b475ad4ad862f2cf9a9
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2026-02-13 23:55:04 +0530

    switch statement example

---
+++ frontend/JS/Switch Statement/app.js
@@ -0,0 +1,44 @@
+let color = "red";
+
+switch (color) {
+    case "red":
+        console.log("The color is red");
+        break;
+    case "blue":
+        console.log("The color is blue");
+        break;
+    case "green":
+        console.log("The color is green");
+        break;
+    default:
+        console.log("The color is not red, blue, or green");
+}
+
+
+let day = 1;
+
+switch (day) {
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+        break;
+    default:
+        console.log("Invalid day number");
+}
